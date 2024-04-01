03adedfc6fea48a415fb8f959f1fb0f2aabccffa
Alex Silva <alexsaraujo92@gmail.com>
2024-03-31 22:51:54 -0300

    add palindrome

---
+++ palindrome/Program.cs
@@ -0,0 +1,13 @@
+﻿bool CheckIfInputArePalindrome(string input)
+{
+    var replaced = input.ToLower();
+    char[] caracteres = replaced.ToCharArray();
+    Array.Reverse(caracteres);
+    var reverse = new string(caracteres);
+    
+    if(replaced == reverse) return true;
+
+    return false;
+}
+
+//Console.WriteLine(CheckIfInputArePalindrome("arara"));
